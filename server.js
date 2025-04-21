c5d14759cee9d73b2c6c16b28b3281107ace5e82
Abdelrahman_Abass <abdoabass400@gmail.com>
2025-04-21 15:39:47 +0200

    edit server naming

--- server.js
+++ server.js
@@ -1,80 +1,80 @@
-// import express from 'express';
-// import cors from 'cors';
-// import {app} from './app.js';
+import express from 'express';
+import cors from 'cors';
+import {app} from './app.js';
+
+const PORT = process.env.PORT || 5000;
+// List of allowed origins
+const allowedOrigins = [
+  // 'http://localhost:3000', // Local development
+  // // 'https://art-market-fbss.vercel.app', // Production
+  // 'http://localhost:5173', // Another allowed domain
+  "*"
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // If the origin is in the list of allowed origins or no origin (like in Postman requests), allow it
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Cache-Control',
+      'Expires',
+      'Pragma',
+    ],
+    credentials: false,
+  })
+);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express on Vercel!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+// import { createServer } from 'http';
+// import { Server } from 'socket.io';
+// import { app } from './app.js';
+// // import  setupSocket  from './socket/index.js';
+// // import { setupSocket } from './socket/index.js';
 
 // const PORT = process.env.PORT || 5000;
-// // List of allowed origins
-// const allowedOrigins = [
-//   // 'http://localhost:3000', // Local development
-//   // // 'https://art-market-fbss.vercel.app', // Production
-//   // 'http://localhost:5173', // Another allowed domain
-//   "*"
-// ];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       // If the origin is in the list of allowed origins or no origin (like in Postman requests), allow it
-//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     },
+// // const server = createServer(app);
 
-//     methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-//     allowedHeaders: [
-//       'Content-Type',
-//       'Authorization',
-//       'Cache-Control',
-//       'Expires',
-//       'Pragma',
-//     ],
-//     credentials: false,
-//   })
-// );
+// // const io = new Server(server, {
+// //   cors: {
+// //     origin: [
+// //       'http://localhost:3000',
+// //       'http://localhost:5173',
+// //       '*',
+// //     ],
+// //     methods: ['GET', 'POST'],
+// //     allowedHeaders: ['Content-Type', 'Authorization'],
+// //     credentials: false,
+// //   },
+// // });
 
+// // export { io };
 
-
-// app.get('/', (req, res) => {
-//   res.send('Hello from Express on Vercel!');
-// });
-
+// // // Set up Socket.IO handlers
+// // setupSocket();
 
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
-
-
-
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import { app } from './app.js';
-// import  setupSocket  from './socket/index.js';
-// import { setupSocket } from './socket/index.js';
-
-const PORT = process.env.PORT || 5000;
-
-// const server = createServer(app);
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: [
-//       'http://localhost:3000',
-//       'http://localhost:5173',
-//       '*',
-//     ],
-//     methods: ['GET', 'POST'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: false,
-//   },
-// });
-
-// export { io };
-
-// // Set up Socket.IO handlers
-// setupSocket();
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
